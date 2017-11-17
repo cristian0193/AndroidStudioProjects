2686436d4f5d2ed349034ce527a5a15140f38d69
Christian Rodriguez <cristian010193@hotmail.com>
2017-11-17 15:09:29 -0500

    Login + Toolbar + Tab
    Creacion de Login + Registro + Recuperar Pass en Firebase
    Toolbar o pestañas
    Tab con fragment asociados

--- ListaTabHost/app/src/main/java/com/example/crodriguez/listatabhost/MainActivity.java
+++ ListaTabHost/app/src/main/java/com/example/crodriguez/listatabhost/MainActivity.java
@@ -1,12 +1,10 @@
-package com.example.crodriguez.questionandroidproject.vista.activities;
+package com.example.crodriguez.listatabhost;
 
-import android.content.Intent;
+import android.support.design.widget.TabLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
-import android.support.design.widget.TabLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
@@ -18,9 +16,9 @@ import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.crodriguez.questionandroidproject.R;
+import android.widget.TextView;
 
-public class MenuActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity {
 
     private SectionsPagerAdapter mSectionsPagerAdapter;
     private ViewPager mViewPager;
@@ -28,21 +26,28 @@ public class MenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_menu);
+        setContentView(R.layout.activity_main);
 
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
 
         mViewPager = (ViewPager) findViewById(R.id.container);
         mViewPager.setAdapter(mSectionsPagerAdapter);
 
-        //Manejo de las opciones para cambio de fragment al view
         TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
 
         mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
         tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
 
     }
 
@@ -50,7 +55,7 @@ public class MenuActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
 
-        getMenuInflater().inflate(R.menu.menu_bar, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -59,10 +64,8 @@ public class MenuActivity extends AppCompatActivity {
 
         int id = item.getItemId();
 
-        if(id == R.id.accion_salir){
-            Intent intent = new Intent(this, PrincipalActivity.class);
-            startActivity(intent);
-            finish();
+        if (id == R.id.action_settings) {
+            return true;
         }
 
         return super.onOptionsItemSelected(item);
@@ -85,19 +88,23 @@ public class MenuActivity extends AppCompatActivity {
         }
 
         @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState) {
-
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
             if(getArguments().getInt(ARG_SECTION_NUMBER) == 1){
-                View rootView = inflater.inflate(R.layout.fragment_preguntas, container, false);
+                View rootView = inflater.inflate(R.layout.fragment_pagina1, container, false);
                 return rootView;
             }else if(getArguments().getInt(ARG_SECTION_NUMBER) == 2){
-                View rootView = inflater.inflate(R.layout.fragment_mis_preguntas, container, false);
+                View rootView = inflater.inflate(R.layout.fragment_pagina2, container, false);
+                return rootView;
+            }else{
+                View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+                TextView textView = (TextView) rootView.findViewById(R.id.section_label);
+                textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
                 return rootView;
             }
-            return null;
+
         }
     }
-
 
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
@@ -113,7 +120,6 @@ public class MenuActivity extends AppCompatActivity {
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
             return 3;
         }
 
---
+++ ListaTabHost/app/src/main/java/com/example/crodriguez/listatabhost/Pagina1Fragment.java
@@ -0,0 +1,59 @@
+package com.example.crodriguez.listatabhost;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class Pagina1Fragment extends Fragment {
+
+
+    private OnFragmentInteractionListener mListener;
+
+    public Pagina1Fragment() {
+
+    }
+
+    public static Pagina1Fragment newInstance() {
+        Pagina1Fragment fragment = new Pagina1Fragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_pagina1, container, false);
+        return view;
+    }
+
+    public void onButtonPressed(Uri uri) {
+        if (mListener != null) {
+            mListener.onFragmentInteraction(uri);
+        }
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        if (context instanceof OnFragmentInteractionListener) {
+            mListener = (OnFragmentInteractionListener) context;
+        } else {
+            throw new RuntimeException(context.toString()
+                    + " must implement OnFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    public interface OnFragmentInteractionListener {
+        void onFragmentInteraction(Uri uri);
+    }
+}
